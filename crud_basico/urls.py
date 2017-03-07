6411ff123e76246e76209c9b151f0423c6f502c1
Gustavo van Erven <gvanerven@gmail.com>
2017-03-06 22:17:05 -0300

    plano de ensino

--- crud_basico/urls.py
+++ crud_basico/urls.py
@@ -4,4 +4,5 @@ from crud_basico import views
 
 urlpatterns = [
     url(r'^$', views.index, name='crud_basico/index'),
+    url(r'^api/filmes$', views.filmes, name='crud_basico/filmes'),
 ]
--- crud_basico/views.py
+++ crud_basico/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 def index(request):
     return render(request, 'crud_basico.html', {})
+
+def filmes(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
