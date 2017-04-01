3909ee50319e4e4d1dda18983cfe7826815a43fb
Gustavo van Erven <gvanerven@gmail.com>
2017-04-01 19:16:39 -0300

    index

--- crud_basico/urls.py
+++ crud_basico/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from crud_basico import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='crud_basico/index'),
+    url(r'^$', views.index, name='crudbasico/index'),
     url(r'^api/filmes$', views.filmes, name='crudbasico/filmes'),
 ]
